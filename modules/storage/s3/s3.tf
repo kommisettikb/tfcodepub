05f0abd40f371e1fd967e7b3d3fd894187168816
kommisettikb <kommisetti76808@gmail.com>
2025-09-24 18:43:23 +0530

    addings3

---
+++ modules/storage/s3/s3.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-12345"                        
+
+  tags = {
+    Name        = "MyS3Bucket"
+  }
+}
