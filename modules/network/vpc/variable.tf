7e3f4dd193a07d7a62a2e432a2d82cb420bb795f
kommisettikb <kommisetti76808@gmail.com>
2025-09-25 11:53:28 +0530

    only using

--- modules/network/vpc/variable.tf
+++ modules/network/vpc/variable.tf
@@ -1,7 +1,7 @@
-#variable "vpc_cidr" {
-  #type        = string
- # default     = "10.0.0.0/16"
-#}
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
 #variable "sub_cidr"{
 #    type = map
