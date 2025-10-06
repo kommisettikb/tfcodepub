946ff901787e52c559185e74a450b77f4a1a2f7c
kommisettikb <kommisetti76808@gmail.com>
2025-10-06 14:33:47 +0530

    modified

--- modules/compute/ec2/ec2.tf
+++ modules/compute/ec2/ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "i3" {
   ami           = "ami-0f9708d1cd2cfee41"  
   instance_type = "t3.micro"
-  subnet_id     = subnet-031939f2ea993e91f
+  subnet_id     = "subnet-031939f2ea993e91f"
 
   tags = {
     Name = "i360"
