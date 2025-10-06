e17cbc4b6664af326b46657fd9d24a108ba52ee1
kommisettikb <kommisetti76808@gmail.com>
2025-10-06 14:27:41 +0530

    modified

--- modules/compute/ebs.tf
+++ modules/compute/ebs.tf
@@ -1,16 +1,16 @@
 # EBS volume creation
-resource "aws_ebs_volume" "eb" {
-  availability_zone = "ap-south-1a"
-  size              = 10
-  tags = {
-    Name = "i360_volume"
-  }
-}
+#resource "aws_ebs_volume" "eb" {
+ # availability_zone = "ap-south-1a"
+  #size              = 10
+  #tags = {
+   # Name = "i360_volume"
+  #}
+#}
 
 #attaching the ebs volume in ap-south-1a
 
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.eb.id
-  instance_id = aaws_instance.i3.id
-}
+#resource "aws_volume_attachment" "ebs_att" {
+ # device_name = "/dev/sdh"
+  #volume_id   = aws_ebs_volume.eb.id
+  #instance_id = aaws_instance.i3.id
+#}
--- modules/compute/ec2/alb.tf
+++ modules/compute/ec2/alb.tf
@@ -1,144 +1,146 @@
 # --- Security Groups ---
-resource "aws_security_group" "alb_sg" {
-  name   = "alb-sg"
-  vpc_id = aws_vpc.main.id
+#resource "aws_security_group" "alb_sg" {
+ # name   = "alb-sg"
+  #vpc_id = aws_vpc.main.id
  
-  ingress {
-    description = "HTTP from anywhere"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  #ingress {
+   # description = "HTTP from anywhere"
+    #from_port   = 80
+    #to_port     = 80
+#    protocol    = "tcp"
+ #   cidr_blocks = ["0.0.0.0/0"]
+  #}
  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#  egress {
+ #   from_port   = 0
+  #  to_port     = 0
+   # protocol    = "-1"
+    #cidr_blocks = ["0.0.0.0/0"]
+#  }
+#}
  
-resource "aws_security_group" "instance_sg" {
-  name   = "instance-sg"
-  vpc_id = aws_vpc.main.id
+#resource "aws_security_group" "instance_sg" {
+ # name   = "instance-sg"
+  #vpc_id = aws_vpc.main.id
  
-  ingress {
-    description = "Allow HTTP from ALB"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
+  # ingress {
+    # description = "Allow HTTP from ALB"
+     #from_port   = 80
+ #    to_port     = 80
+   #  protocol    = "tcp"
+    # security_groups = [aws_security_group.alb_sg.id]
+  # }
  
-  ingress {
-    description = "SSH (not recommended for prod, wide open here)"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+   #ingress {
+     #description = "SSH (not recommended for prod, wide open here)"
+     #from_port   = 22
+     #to_port     = 22
+     #protocol    = "tcp"
+     #cidr_blocks = ["0.0.0.0/0"]
+   #}
  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+   #egress {
+     #from_port   = 0
+     #to_port     = 0
+     #protocol    = "-1"
+     #cidr_blocks = ["0.0.0.0/0"]
+   #}
+ #}
  
 # --- ALB ---
-resource "aws_lb" "app_lb" {
-  name               = "my-app-alb"
-  load_balancer_type = "application"
-  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-  security_groups    = [aws_security_group.alb_sg.id]
-}
+ #resource "aws_lb" "app_lb" {
+  # name               = "my-app-alb"
+  #load_balancer_type = "application"
+ # subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  #security_groups    = [aws_security_group.alb_sg.id]
+#}
  
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+#resource "aws_lb_target_group" "app_tg" {
+ # name     = "app-tg"
+  #port     = 80
+ # protocol = "HTTP"
+  #vpc_id   = aws_vpc.main.id
  
-  health_check {
-    path                = "/"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    matcher             = "200-299"
-  }
-}
+  #health_check {
+   # path                = "/"
+    #interval            = 30
+#    timeout             = 5
+ #   healthy_threshold   = 2
+  #  unhealthy_threshold = 2
+   # matcher             = "200-299"
+  #}
+#}
  
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 80
-  protocol          = "HTTP"
+#resource "aws_lb_listener" "http" {
+ # load_balancer_arn = aws_lb.app_lb.arn
+  #port              = 80
+  #protocol          = "HTTP"
  
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
+  #default_action {
+   # type             = "forward"
+   # target_group_arn = aws_lb_target_group.app_tg.arn
+  #}
+#}
  
 # --- EC2 Instances ---
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+#data "aws_ami" "amazon_linux" {
+ # most_recent = true
+  #owners      = ["amazon"]
+  #filter {
+   # name   = "name"
+    #values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  #}
+#}
  
-resource "aws_instance" "web1" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public_a.id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  associate_public_ip_address = true
+#resource "aws_instance" "web1" {
+ # ami                    = data.aws_ami.amazon_linux.id
+  #instance_type          = "t3.micro"
+  #subnet_id              = aws_subnet.public_a.id
+  #vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  #associate_public_ip_address = true
  
-  user_data = <<-EOF
+  #user_data = <<-EOF
+   #           #!/bin/bash
+    #          yum update -y
+      #        amazon-linux-extras install -y nginx1
+     #         systemctl enable nginx
+        #      systemctl start nginx
+       #       echo "Hello from web1" > /usr/share/nginx/html/index.html
+         #     EOF
+ 
+#  tags = { Name = "web1" }
+#}
+ 
+#resource "aws_instance" "web2" {
+ # ami                    = data.aws_ami.amazon_linux.id
+  #instance_type          = "t3.micro"
+  #subnet_id              = aws_subnet.public_b.id
+  #vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  #associate_public_ip_address = true
+ 
+  #user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              amazon-linux-extras install -y nginx1
-              systemctl enable nginx
-              systemctl start nginx
-              echo "Hello from web1" > /usr/share/nginx/html/index.html
-              EOF
+   #           yum update -y
+    #          amazon-linux-extras install -y nginx1
+     #         systemctl enable nginx
+      #        systemctl start nginx
+       #       echo "Hello from web2" > /usr/share/nginx/html/index.html
+        #      EOF
  
-  tags = { Name = "web1" }
-}
- 
-resource "aws_instance" "web2" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public_b.id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  associate_public_ip_address = true
- 
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install -y nginx1
-              systemctl enable nginx
-              systemctl start nginx
-              echo "Hello from web2" > /usr/share/nginx/html/index.html
-              EOF
- 
-  tags = { Name = "web2" }
-}
+#  tags = { Name = "web2" }
+#}
  
 # Attach instances to Target Group
-resource "aws_lb_target_group_attachment" "web1" {
-  target_group_arn = aws_lb_target_group.app_tg.arn
-  target_id        = aws_instance.web1.id
-  port             = 80
-}
+#resource "aws_lb_target_group_attachment" "web1" {
+ # target_group_arn = aws_lb_target_group.app_tg.arn
+  #target_id        = aws_instance.web1.id
+  #port             = 80
+#}
  
-resource "aws_lb_target_group_attachment" "web2" {
-  target_group_arn = aws_lb_target_group.app_tg.arn
-  target_id        = aws_instance.web2.id
-  port             = 80
-}
+#resource "aws_lb_target_group_attachment" "web2" {
+ # target_group_arn = aws_lb_target_group.app_tg.arn
+  #target_id        = aws_instance.web2.id
+  #port             = 80
+#}
+
+
