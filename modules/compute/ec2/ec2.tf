f39ae1e781784e48bc9d3fbfcbba18dc85091bd2
kommisettikb <kommisetti76808@gmail.com>
2025-10-06 14:07:13 +0530

    adding ec2 details

--- modules/compute/ec2/ec2.tf
+++ modules/compute/ec2/ec2.tf
@@ -1,8 +1,8 @@
 # Use the first subnet for simplicity
 resource "aws_instance" "i3" {
-  ami           = "ami-01b6d88af12965bb6"  
+  ami           = "ami-0f9708d1cd2cfee41"  
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_subnet.id 
+  subnet_id     = "subnet-031939f2ea993e91f" 
 
   tags = {
     Name = "i360"
